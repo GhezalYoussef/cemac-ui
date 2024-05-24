87550c4f27c060ba38def1666d6fa568dd549d46
PYGL07231 <ext.youssef.ghezal@sncf.fr>
2024-05-24 15:34:38 +0200

    modifier menu !

--- src/app/pages/mode-analyse/mode-analyse.component.ts
+++ src/app/pages/mode-analyse/mode-analyse.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import {TemplateModule} from "@tec/condor/components";
+import {PanelModule} from "primeng/panel";
+import {InputNumberModule} from "primeng/inputnumber";
+import {AutoCompleteModule} from "primeng/autocomplete";
+import {DropdownModule} from "primeng/dropdown";
+import {MultiSelectModule} from "primeng/multiselect";
+import {ToggleButtonModule} from "primeng/togglebutton";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-mode-analyse',
   standalone: true,
-  imports: [],
+  imports: [
+    TemplateModule,
+    PanelModule,
+    InputNumberModule,
+    AutoCompleteModule,
+    DropdownModule,
+    MultiSelectModule,
+    ToggleButtonModule,
+    ButtonModule
+  ],
   templateUrl: './mode-analyse.component.html',
   styleUrl: './mode-analyse.component.scss'
 })
--- src/app/pages/mode-saisie/mode-saisie.component.ts
+++ src/app/pages/mode-saisie/mode-saisie.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import {AutoCompleteModule} from "primeng/autocomplete";
+import {ButtonModule} from "primeng/button";
+import {DropdownModule} from "primeng/dropdown";
+import {InputNumberModule} from "primeng/inputnumber";
+import {MultiSelectModule} from "primeng/multiselect";
+import {PanelModule} from "primeng/panel";
+import {ConfirmationService, MessageService, SharedModule} from "primeng/api";
+import {TemplateModule} from "@tec/condor/components";
+import {ToggleButtonModule} from "primeng/togglebutton";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ELigne} from "../../models/enum/ELigne.enum";
+import {InputTextModule} from "primeng/inputtext";
+import {ETension} from "../../models/enum/ETension.enum";
 
 @Component({
   selector: 'app-mode-saisie',
   standalone: true,
-  imports: [],
+    imports: [
+        AutoCompleteModule,
+        ButtonModule,
+        DropdownModule,
+        InputNumberModule,
+        MultiSelectModule,
+        PanelModule,
+        SharedModule,
+        TemplateModule,
+        ToggleButtonModule,
+        ReactiveFormsModule,
+        InputTextModule
+    ],
   templateUrl: './mode-saisie.component.html',
   styleUrl: './mode-saisie.component.scss'
 })
 export class ModeSaisieComponent {
+
+    formSaisie ?: FormGroup;
+    typeLigneList : string[] = [ELigne.CLASSIQUE, ELigne.LGV];
+    typeInstallationTensionList : string[] = [ETension.ALL, ETension._1500, ETension._25000]
+
+    constructor(private formBuilder: FormBuilder,
+                private route: ActivatedRoute,
+                private router: Router,
+                private messageService: MessageService) {
+
+        this.formSaisie = this.formBuilder.group({
+            typeLigne:[],
+            nbrPantoMin:[],
+            nbrPantoMax:[],
+            vitesseMin:[],
+            vitesseMax:[],
+            categorieMaintenance:[],
+            typeInstallationTension:[],
+            nombreML:[],
+            nombreIS:[],
+            nombreAIG:[],
+            nombreAT:[],
+            nombreIA:[]
+        });
+    }
 
 }
