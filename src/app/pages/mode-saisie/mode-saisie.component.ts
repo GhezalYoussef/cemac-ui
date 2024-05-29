71e53b8609d468e8cdea2f95ce8b26ae70233b4c
PYGL07231 <ext.youssef.ghezal@sncf.fr>
2024-05-29 19:00:53 +0200

    add module Analyse !

--- src/app/pages/mode-saisie/mode-saisie.component.ts
+++ src/app/pages/mode-saisie/mode-saisie.component.ts
@@ -5,7 +5,7 @@ import {DropdownModule} from "primeng/dropdown";
 import {InputNumberModule} from "primeng/inputnumber";
 import {MultiSelectModule} from "primeng/multiselect";
 import {PanelModule} from "primeng/panel";
-import {MessageService, SharedModule} from "primeng/api";
+import {ConfirmationService, MessageService, SharedModule} from "primeng/api";
 import {TemplateModule} from "@tec/condor/components";
 import {ToggleButtonModule} from "primeng/togglebutton";
 import { CommonModule } from '@angular/common';
@@ -26,6 +26,12 @@ import {Catenaire} from "../../models/catenaire.model";
 import {CatenaireService} from "../../services/catenaire.service";
 import {FamilleCatenaireService} from "../../services/famille-catenaire.service";
 import {FamilleCatenaire} from "../../models/famille-catenaire.model";
+import {Requete} from "../../models/requete";
+import {catchError, of, switchMap, throwError} from "rxjs";
+import {RequeteService} from "../../services/requete.service";
+import {NavigationService} from "../../services/navigation.service";
+import {ToastModule} from "primeng/toast";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
 
 @Component({
   selector: 'app-mode-saisie',
@@ -44,8 +50,11 @@ import {FamilleCatenaire} from "../../models/famille-catenaire.model";
         ReactiveFormsModule,
         InputTextModule,
         StyleClassModule,
-        MessageModule
+        MessageModule,
+        ToastModule,
+        ConfirmDialogModule
     ],
+    providers: [ConfirmationService],
   templateUrl: './mode-saisie.component.html',
   styleUrl: './mode-saisie.component.scss'
 })
@@ -57,6 +66,7 @@ export class ModeSaisieComponent implements OnInit {
     catenaireInstallationList : Catenaire[] = [];
     familleCatenaireInstallationList : FamilleCatenaire[] = [];
     catenaireInstallationListFilter : Catenaire[] = [];
+    public requete ?: Requete;
 
     constructor(private formBuilder: FormBuilder,
                 private route: ActivatedRoute,
@@ -64,10 +74,13 @@ export class ModeSaisieComponent implements OnInit {
                 private messageService: MessageService,
                 private categorieMaintenanceService: CategorieMaintenanceService,
                 private catenaireService: CatenaireService,
-                private familleCatenaireService: FamilleCatenaireService) {
+                private familleCatenaireService: FamilleCatenaireService,
+                private requeteService:RequeteService,
+                private navigationService:NavigationService,
+                private confirmationService: ConfirmationService) {
 
         this.formSaisie = this.formBuilder.group({
-            typeLigne:['',Validators.required],
+            typeLigne:[ELigne.LGV,Validators.required],
             nbrPanto:[0, Validators.required],
             vitesse:[0,Validators.required],
             categorieMaintenance:['',Validators.required],
@@ -133,6 +146,84 @@ export class ModeSaisieComponent implements OnInit {
             catenaireList  => catenaireList.familleCatenaire === this.f.FamilleInstallationTension.value.id);
     }
 
+    onClickSave(analyse:boolean) {
+        this.confirmationService.confirm({
+            message: `Vous êtes sur le point d'ajouter la requete <br/>Souhaitez-vous confirmer?`,
+            header: `Confirmation de l'ajout de la requete`,
+            icon: 'pi pi-info-circle',
+            accept: () => {
+                this.saveRequete(analyse);
+            }
+        });
+    }
 
+    public saveRequete(analyse: boolean) {
+        const maDate: Date = new Date();
+        const requete = {
+            id: this.requete ? this.requete.id : null,
+            requeteRef: this.requete ? this.requete.requeteRef : this.formatDate(maDate),
+            dateCreation: '',
+            createur: '',
+            dateModification: '',
+            modificateur: '',
+            typeLigne:this.f.typeLigne.value,
+            nbrPanto:this.f.nbrPanto.value,
+            vitesse:this.f.vitesse.value,
+            categorieMaintenance:this.f.categorieMaintenance.value,
+            typeInstallationTension:this.f.typeInstallationTension.value,
+            nombreML:this.f.nombreML.value,
+            nombreIS:this.f.nombreIS.value,
+            nombreAIG:this.f.nombreAIG.value,
+            nombreAT:this.f.nombreAT.value,
+            nombreIA:this.f.nombreIA.value,
+            analyseResultList: []
+        };
 
+        this.requeteService.saveRequete(requete).pipe(
+            switchMap((res) => {
+                return of(res);
+            }),
+            catchError((error) => {
+                // Gérer l'erreur ici
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Sauvegarder',
+                    detail: `Erreur lors de la sauvegarde de la requête: ${error}`,
+                    key: 'top'
+                });
+                return throwError(error);
+            })
+        ).subscribe((res) => {
+            // Succès : afficher un message de succès et naviguer vers la liste des requêtes
+            this.messageService.add({
+                severity: 'success',
+                summary: 'Sauvegarder',
+                detail: `Requête sauvegardée sous la référence ${requete.requeteRef}`,
+                key: 'top'
+            });
+            if(analyse){
+                this.navigationService.navigationToAnalyseRequete(res)
+            }else{
+                this.navigationService.navigateToListRequetes();
+            }
+        });
+    }
+
+    formatDate(date: Date): string {
+        const day: number = date.getDate();
+        const month: number = date.getMonth() + 1; // Les mois commencent à 0
+        const year: number = date.getFullYear();
+        const hours: number = date.getHours();
+        const minutes: number = date.getMinutes();
+        const seconds: number = date.getSeconds();
+
+        // Formatage avec zéro devant les chiffres simples
+        const formattedDate = `CEMAC_${year}${this.addLeadingZero(month)}${this.addLeadingZero(day)}${this.addLeadingZero(hours)}${this.addLeadingZero(minutes)}${this.addLeadingZero(seconds)}`;
+
+        return formattedDate;
+    }
+
+    addLeadingZero(value: number): string {
+        return value < 10 ? `0${value}` : `${value}`;
+    }
 }
